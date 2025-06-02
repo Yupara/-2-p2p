00bc056e3e2ae55d0fdc4e9e909aaf60e15e0616
Yupara <parviz.yusupov03@mail.ru>
2025-06-02 05:09:19 +0500

    Update Message.js

--- server/models/Message.js
+++ server/models/Message.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
   offerId: String,
+  userId: String,
   message: String,
   timestamp: { type: Date, default: Date.now }
 });
