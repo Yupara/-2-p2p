10c256a70c4c322ba4d83bf86c8ed41a00c39512
Yupara <parviz.yusupov03@mail.ru>
2025-06-02 05:12:39 +0500

    Create Offer.js

---
+++ server/Offer.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const OfferSchema = new mongoose.Schema({
+  sellCurrency: String,
+  sellAmount: Number,
+  buyCurrency: String,
+  paymentMethod: String,
+  contact: String,
+  userId: String,
+  status: { type: String, default: 'active' }, // active, disputed, completed
+  commissionPaid: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Offer', OfferSchema);
